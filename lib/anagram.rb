131e0b62b059875637241a6af20750dec275acb4
Shami Tomita <shamitomita@gmail.com>
2021-01-27 16:38:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,9 @@ class Anagram
   def match(word)
     #take an array of possible anagrams and should 1. return all matches into an array, 2.return an empty array if  there are no matches 
     #so this method should take in the word upon initialization and compare to the array 
-    @@array.detect
+    matcher = word.split("") #this is where we convert our string to an array so it can be matched 
+    @@array.detect each do |x| 
+      x.split("")
   end 
   
   

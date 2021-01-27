c6cf6efe9625130572d7d492a6e1cc8e57cdc00a
Shami Tomita <shamitomita@gmail.com>
2021-01-27 16:31:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,8 @@ class Anagram
   end 
   
   def match
-    #take an array of possible anagrams and should 1. return all matches into an array, 2.return an empty array if  
+    #take an array of possible anagrams and should 1. return all matches into an array, 2.return an empty array if  there are no matches 
+    #so this method should take in an array 
   end 
   
   

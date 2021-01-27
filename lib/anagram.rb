cd9edfb40c7bb648ab3221b16a9baa4d6d039180
Shami Tomita <shamitomita@gmail.com>
2021-01-27 16:44:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,6 +10,7 @@ class Anagram
   
   def match(array)
     matching_array = []
+    matching_word = @word.split("")
     #take an array of possible anagrams and should 1. return all matches into an array, 2.return an empty array if  there are no matches 
     #so this method should take in the word upon initialization and compare to the array 
     #matcher = @word.split("") #this is where we convert our string to an array so it can be matched 

12519ef6f20cf0cc84b8dc8cb1b4548d6fa8eb6a
Shami Tomita <shamitomita@gmail.com>
2021-01-27 16:32:54 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,7 @@
 # Your code goes here!
 
 class Anagram
- attr_accessor :word
+ attr_accessor :word, :array
   
   def initialize
     
@@ -9,7 +9,7 @@ class Anagram
   
   def match
     #take an array of possible anagrams and should 1. return all matches into an array, 2.return an empty array if  there are no matches 
-    #so this method should take in an array 
+    #so this method should take in a word 
   end 
   
   

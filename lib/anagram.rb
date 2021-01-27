8538e5da8b1d383090118edf6b1fe672efd44588
Shami Tomita <shamitomita@gmail.com>
2021-01-27 16:42:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,10 @@ class Anagram
     @word = word  
   end 
   
-  def match(word)
+  def match(array)
     #take an array of possible anagrams and should 1. return all matches into an array, 2.return an empty array if  there are no matches 
     #so this method should take in the word upon initialization and compare to the array 
-    matcher = word.split("") #this is where we convert our string to an array so it can be matched 
+    matcher = @word.split("") #this is where we convert our string to an array so it can be matched 
     @@array.detect each do |x| #go through the array we have 
       matchee = x.split("")
       if matcher.sort == matchee.sort 

e6ec7b98dad2346b2107a37cc8f6f2a2638eaa2a
Shami Tomita <shamitomita@gmail.com>
2021-01-27 16:35:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,7 +11,7 @@ class Anagram
   def match(word)
     #take an array of possible anagrams and should 1. return all matches into an array, 2.return an empty array if  there are no matches 
     #so this method should take in the word upon initialization and compare to the array 
-    
+    @@array.detect
   end 
   
   
